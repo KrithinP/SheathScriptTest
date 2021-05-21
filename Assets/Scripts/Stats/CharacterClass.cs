ac33e0a39a16f00c67910709f0c7f91957cb17fd
KrithinP <76639820+KrithinP@users.noreply.github.com>
2021-05-21 15:31:01 +0530

    Git Commit 2

---
+++ Assets/Scripts/Stats/CharacterClass.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    enum CharacterClass
+    {
+        Knight,
+        Wizards,
+        Grunt,
+        Soldier,
+        Foz,
+        Wolf,
+        Archer
+    }
+}
